dbb0b7292c4ef16fc18aec5b9ca0e9ded0bf2993
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-22 11:14:06 +0300

    fetched login data from db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -7,7 +9,32 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
-exports.postLogin = (req, res, next) => {};
+exports.postLogin = (req, res, next) => {
+  const username = req.body.username;
+  const pin = req.body.pin;
+  //   const userId = req.body.user._id;
+
+  User.findOne({ username: username })
+    .then(user => {
+      if (!user) {
+        return res.redirect('/login');
+      }
+      return bcrypt
+        .compare(pin, user.pin)
+        .then(doMatch => {
+          if (doMatch) {
+            console.log('logged in');
+            return res.redirect('/');
+          }
+          console.log('password mismatch');
+          return res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => console.log(err));
+};
 
 exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
@@ -21,12 +48,18 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const pin = req.body.pin;
 
-  const user = new User({
-    username,
-    email,
-    pin,
-  });
-  user.save().then(() => {
-    res.redirect('/login');
-  });
+  bcrypt
+    .hash(pin, 12)
+    .then(hashedPin => {
+      const user = new User({
+        username,
+        email,
+        pin: hashedPin,
+      });
+      return user.save();
+    })
+    .then(() => {
+      res.redirect('/login');
+    })
+    .catch(err => console.log(err));
 };
