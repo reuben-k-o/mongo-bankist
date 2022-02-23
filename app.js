af469c5e095cd704440f98dabc0e96ea0fd4280b
reuben-k-o <rubenkhaemba@gmail.com>
2022-02-23 11:44:50 +0300

    sessions

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const bankRoutes = require('./routes/bank');
 const authRoutes = require('./routes/auth');
 const rootDir = require('./util/path');
+
+const MONGODB_URI =
+  'mongodb+srv://Reubenk:Reuben11*@cluster0.vnlvk.mongodb.net/bankist';
+
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 const app = express();
 
@@ -15,13 +25,36 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
+
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id).then(user => {
+    req.user = user;
+    return next();
+  });
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+});
 
 app.use(bankRoutes);
 app.use(authRoutes);
 
 mongoose
-  .connect('mongodb+srv://Reubenk:Reuben11*@cluster0.vnlvk.mongodb.net/bankist')
+  .connect(MONGODB_URI)
   .then(result => {
     app.listen(3000);
   })
--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,18 @@ const { validationResult } = require('express-validator');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     isAuthenticated: false,
     hasErrors: false,
-    errorMessage: null,
+    errorMessage: message,
     validationErrors: [],
   });
 };
